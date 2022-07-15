eef32280c241a5b158d3efe74cd007aea5b1b3e7
SangHun <shzero211@naver.com>
2022-07-15 10:57:12 +0900

    셋팅

---
+++ src/main/java/com/ll/array/ArrayList.java
@@ -0,0 +1,5 @@
+package com.ll.array;
+
+public class ArrayList {
+
+}
---
+++ src/test/java/com/ll/array/ArrayListTest.java
@@ -0,0 +1,12 @@
+package com.ll.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+@Test
+public void  ArrayList_클래스가_존재한다(){
+    ArrayList al=new ArrayList();
+}
+}
