7fcdbb936bf8c707559df6677da9b9955defb903
SangHun <shzero211@naver.com>
2022-07-15 11:40:47 +0900

    완성

--- src/main/java/com/ll/array/ArrayList.java
+++ src/main/java/com/ll/array/ArrayList.java
@@ -1,5 +1,68 @@
 package com.ll.array;
 
 public class ArrayList {
+    int[] datas;
+    int lastIdx;
+    ArrayList(){
+        datas=new int[2];
+        lastIdx=-1;
+    }
+    public int size() {
+        return lastIdx+1;
+    }
 
+    public void add(int data) {
+        sizeUpIfFull();
+        datas[++lastIdx]=data;
+    }
+
+    private void sizeUpIfFull() {
+        if(isFull()){
+            sizeUp();
+        }
+    }
+
+    private void sizeUp() {
+        int[] temp=new int[datas.length*2];
+        for(int i=0;i<datas.length;i++){
+            temp[i]=datas[i];
+        }
+        datas=temp;
+    }
+
+    private boolean isFull() {
+        return lastIdx+1>=datas.length?true: false;
+    }
+
+    public int get(int idx) {
+        return datas[idx];
+    }
+
+    public void removeAt(int idx) {
+        for(int i=idx+1;i<=lastIdx;i++){
+            datas[i-1]=datas[i];
+        }
+        lastIdx--;
+    }
+
+    public int getArrayLength() {
+        return datas.length;
+    }
+
+    public void showAllValues() {
+        System.out.println("== 전체 데이터 출력 ==");
+        for(int i=0;i<size();i++){
+            System.out.println(i+" : "+datas[i]);
+        }
+    }
+
+    public void addAt(int idx,int data) {
+        sizeUpIfFull();
+
+        for (int i = size()- 1; i > idx; i--) {
+            datas[i] = datas[i - 1];
+        }
+        datas[idx] = data;
+        lastIdx++;
+    }
 }
--- src/test/java/com/ll/array/ArrayListTest.java
+++ src/test/java/com/ll/array/ArrayListTest.java
@@ -2,6 +2,8 @@ package com.ll.array;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayListTest {
@@ -9,4 +11,126 @@ class ArrayListTest {
 public void  ArrayList_클래스가_존재한다(){
     ArrayList al=new ArrayList();
 }
+    @Test
+    void size() {
+        ArrayList al = new ArrayList();
+        assertEquals(0, al.size());
+    }
+    @Test
+    void add() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+    }
+    @Test
+    void get() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        int rs = al.get(0);
+
+        assertEquals(100, rs);
+    }
+
+    @Test
+    void add를_한번_할_때_마다_size가_증가() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        assertEquals(3, al.size());
+    }
+    @Test
+    void get_1_equals_200() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+        int rs = al.get(1);
+
+        assertEquals(200, rs);
+    }
+    @Test
+    void removeAt() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        al.removeAt(1);
+    }
+    @Test
+    void removeAt을_실행하면_size가_1_줄어든다() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        assertEquals(3, al.size());
+
+        al.removeAt(1);
+
+        assertEquals(2, al.size());
+    }
+    @Test
+    void removeAt을_실행하면_뒷좌석에_있는_데이터들이_전부_한칸씩_앞으로_이동() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        assertEquals(200, al.get(1));
+
+        al.removeAt(1);
+
+        assertEquals(300, al.get(1));
+    }
+    @Test
+    void 최초에_배열의_크기는_2_이다() {
+        ArrayList al = new ArrayList();
+
+        assertEquals(2, al.getArrayLength());
+    }
+    @Test
+    void 배열이_꽉_차면_2배_크기로_증가해야_한다() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        assertEquals(2, al.getArrayLength());
+        al.add(300);
+        assertEquals(4, al.getArrayLength());
+    }
+
+    @Test
+    void showAllValues() {
+        ArrayList al = new ArrayList();
+        al.add(1000);
+        al.add(200);
+        al.add(30);
+
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        al.showAllValues();
+
+        String rs = output.toString();
+
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertTrue(rs.contains("== 전체 데이터 출력 =="));
+        assertTrue(rs.contains("0 : 1000"));
+        assertTrue(rs.contains("1 : 200"));
+        assertTrue(rs.contains("2 : 30"));
+    }
+    @Test
+    public void addAt(){
+    ArrayList al=new ArrayList();
+    al.addAt(0,3);
+        al.addAt(0,4);
+        al.addAt(1,100);
+     assertEquals(4,al.get(0));
+        assertEquals(100,al.get(1));
+    }
+
+
+
+
 }
